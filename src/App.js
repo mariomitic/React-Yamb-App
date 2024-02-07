ab29ed3812362867923a7612d634cd634f6a2c14
Mario Mitic <mariomitic@yahoo.com>
2024-02-06 20:49:47 -0800

    adjusted for phone

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import './components/MainStyle.css'
 import './components/MainStyle_412px.css'
-import './components/MainStyle_768px.css'
+import './components/MainStyle_1600px.css'
 
 import Dices from './components/Dices'
 
